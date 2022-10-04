fadd6f566be7cd40a58268023a5ba61cb52392df
Berlin <Berline_lucien@yahoo.com>
2022-10-03 23:01:21 -0400

    board setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
-import "./App.css";
-
+import "./index.css";
+import CrushBoard from "./Pages/CrushBoard";
 
 function App() {
   return (
-    <div className="App">
+    <div className="crush-main">
       Hello from Techtonica
-    
+    <CrushBoard/>
     </div>
   );
 }
