6f63177044e406df38e961d700e7e6afc34d3f8d
Berlin <Berline_lucien@yahoo.com>
2022-10-03 23:49:09 -0400

    drag feature

--- client/src/Pages/CrushBoard.js
+++ client/src/Pages/CrushBoard.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect } from "react";
 import { useState } from "react";
 import blank from "../images/blank.png";
@@ -24,8 +25,11 @@ const candyColors = [
 const CrushBoard = () => {
   // set color arrangement, passing random colorArrangement into currentColor
   const [currentColorArrangement, setCurrentColorArrangement] = useState([]);
+  const [squareBeingDragged, setSquareBeingDragged] = useState(null);
+  const [squareBeingReplaced, setSquareBeingReplaced] = useState(null);
   const [scoreDisplay, setScoreDisplay] = useState(0);
 
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   const checkForColumnOfFour = () => {
     for (let i = 0; i <= 39; i++) {
       const columnOfFour = [i, i + width, i + width * 2, i + width * 3];
@@ -124,9 +128,40 @@ const CrushBoard = () => {
       }
     }
   };
-    
-    // move us down into the board
-    
+
+  // move us down into the board
+  const moveIntoSquareBelow = () => {
+    for (let i = 0; i <= 55; i++) {
+      const firstRow = [0, 1, 2, 3, 4, 5, 6, 7];
+      const isFirstRow = firstRow.includes(i);
+
+      // generate new candies if the first row is empty
+      // if its a blank generate a random color
+      if (isFirstRow && currentColorArrangement[i] === blank) {
+        let randomNumber = Math.floor(Math.random() * candyColors.length);
+        currentColorArrangement[i] = candyColors[randomNumber];
+      }
+      // squares are moving up to replinsh
+      if (currentColorArrangement[i + width] === blank) {
+        currentColorArrangement[i + width] = currentColorArrangement[i];
+        currentColorArrangement[i] = blank;
+      }
+    }
+  };
+
+  // drag and drop functions
+    // drag start picks up the id of the square
+    const dragStart = (e) => {
+        console.log('start drag')
+      setSquareBeingDragged(e.target)
+  };
+    const dragDrop = (e) => {
+        console.log('drag drop')
+      setSquareBeingReplaced(e.target)
+  };
+    const dragEnd = (e) => {
+      
+  };
 
   // create the board with the colors
   const createBoard = () => {
@@ -151,14 +186,21 @@ const CrushBoard = () => {
 
   useEffect(() => {
     const timer = setInterval(() => {
-        checkForColumnOfFour();
-        checkForRowOfFour();
-        checkForColumnOf_Three();
-        checkForRowOfThree();
+      checkForColumnOfFour();
+      checkForRowOfFour();
+      checkForColumnOf_Three();
+      checkForRowOfThree();
       setCurrentColorArrangement([...currentColorArrangement]);
-    }, 100);
-    return () => clearInterval(timer);
-  }, [checkForColumnOfFour, checkForRowOfFour, checkForColumnOf_Three, checkForRowOfThree]);
+    }, 100)
+    return () => clearInterval(timer)
+  }, [
+    checkForColumnOfFour,
+    checkForRowOfFour,
+    checkForColumnOf_Three,
+    checkForRowOfThree,
+    moveIntoSquareBelow,
+    currentColorArrangement,
+  ]);
 
   return (
     <div className="crush-main">
@@ -168,8 +210,15 @@ const CrushBoard = () => {
           <img
             key={index}
             src={candyColor}
-            style={{ backgroundColor: candyColor }}
             alt={candyColor}
+            data-id={index}
+            draggable={true}
+            onDragStart={dragStart}
+            onDragOver={(e) => e.preventDefault()}
+            onDragEnter={(e) => e.preventDefault()}
+            onDragLeave={(e) => e.preventDefault()}
+            onDrag={dragDrop}
+            onDragEnd={dragEnd}
           />
         ))}
       </div>
